71caff1f4f8542875f194e60831fd33c02ada3e2
Usuario <Usuario@192.168.101.9>
2025-05-10 19:53:59 -0600

    inicial commit

---
+++ grupo6prograb2025/src/main/java/gt/edu/miumg/grupo6prograb2025/Grupo6prograb2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.grupo6prograb2025;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Grupo6prograb2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
